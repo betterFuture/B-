77783706c0492936c1d857fd7fc8d268f58669ce
Dan <cgphaoyun2015@126.com>
2015-06-24 16:15:40 +0800

    删除文件View

---
+++ ExerciseGit/MyView.m
@@ -0,0 +1,21 @@
+//
+//  MyView.m
+//  ExerciseGit
+//
+//  Created by lanouhn on 15/6/24.
+//  Copyright (c) 2015年 cgpLianxi. All rights reserved.
+//
+
+#import "MyView.h"
+
+@implementation MyView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
